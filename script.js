555de288d35784b37873a3d4692d189e57e49600
zmthall <74847979+zmthall@users.noreply.github.com>
2023-08-01 11:40:53 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
-console.log(buttons);
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
         
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
     })
+})
+
+const popup = document.querySelector("[data-carousel-container]");
+const closeButton = document.querySelector("[data-close]");
+const openButton = document.querySelector("[data-open]");
+
+
+closeButton.addEventListener("click", () => {
+    // document.querySelector("[data-main]").classList.toggle("hidden");
+
+    if (document.querySelector("[data-main]").classList.contains("hidden")) {
+        popup.classList.toggle("hidden");
+        document.querySelector("[data-main]").classList.toggle("hidden");
+    } else {
+        popup.classList.toggle("hidden");
+    }
+})
+
+openButton.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+        popup.classList.toggle("hidden");
+        document.querySelector("[data-main]").classList.toggle("hidden");
+    } else {
+        popup.classList.toggle("hidden");
+    }
 })
